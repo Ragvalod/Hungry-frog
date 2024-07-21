b9a694df6aecfced97b71cd1515612dbf13a547c
Ragvalod <konfucy@gmail.com>
2024-07-21 10:20:00 +0300

    Added spawn and move logic

--- Assets/Scripts/Fish/FishMowe.cs
+++ Assets/Scripts/Fish/FishMowe.cs
@@ -2,23 +2,52 @@ using UnityEngine;
 
 public class FishMowe : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _moveToPoints;
+
     [SerializeField] private GameObject _lineToPoint;
-    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _moveSpeedMin = 1;
+    [SerializeField] private float _moveSpeedMax = 100;
     [SerializeField] private float _fishScale;
 
-    private GameObject _targetPointToMove;
-    private Vector3 _targetPosition;
-
+    private Vector3 direction;
+    private Vector3 moveTo;
+    float speed;
     private void Start()
     {
-        _targetPointToMove = _moveToPoints[Random.Range(0, _moveToPoints.Length)];
-        //_targetPosition = new Vector3(_targetPointToMove.transform.position.x, 0 , _targetPointToMove.transform.position.y);
+        
+        speed = Random.Range(5, 50);
+        moveTo = Spawner.Instance.GetRandomPointToMove().transform.position;
+        direction = moveTo - transform.position;
+        direction.Normalize();
+
     }
     private void Update()
     {
-        transform.Translate(_lineToPoint.transform.position * Time.deltaTime * _moveSpeed);
+        Move();
+        Lock();
+        DestroyThisObject();
+
+
     }
 
+    private void Move()
+    {
+       transform.position = Vector3.MoveTowards(transform.position, moveTo, Time.deltaTime * speed);
+    }
+
+    private void Lock()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+
+    }
+
+    private void DestroyThisObject()
+    {
+        if (moveTo == transform.position)
+        {
+            Destroy(gameObject);
+        }
+
+    }
 
 }
--- Assets/Scripts/Fish/Spawner.cs
+++ Assets/Scripts/Fish/Spawner.cs
@@ -1,9 +1,44 @@
+using System.Collections;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] _spawnPoints;
+    [SerializeField] private GameObject _fish;
+    [SerializeField] private float _spawnIntervalSec = 5;
 
+    public static Spawner Instance;
 
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnFishWithInterval());
+    }
+    
+
+    public GameObject GetRandomPointToMove()
+    {
+        GameObject randomGameObject = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+
+        return randomGameObject;
+    }
+
+    private IEnumerator SpawnFishWithInterval()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnIntervalSec); // Интервал в секундах
+
+            GameObject spawnedFish = Instantiate(_fish, GetRandomPointToMove().transform.position, Quaternion.identity);
+            Vector3 spawnPoint = new Vector3(spawnedFish.transform.localScale.x, spawnedFish.transform.localScale.y, spawnedFish.transform.localScale.z);
+            spawnedFish.transform.localScale = spawnPoint * Random.Range(0.5f, 5.0f);
+        }
+    }
 
 }
